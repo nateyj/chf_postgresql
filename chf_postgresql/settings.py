1121c442fe9cc1a6802379e7e40a254fda6f7a59
Johnson <nathan.johnson2@honeywell.com>
2016-06-10 09:46:28 -0400

    Modified settings.py to include PostgreSQL database authentication

--- chf_postgresql/settings.py
+++ chf_postgresql/settings.py
@@ -141,8 +141,12 @@ WSGI_APPLICATION = 'chf_postgresql.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chf',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres@nateyj',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
---
+++ homepage/templates/.cached_templates/base.htm.py
@@ -0,0 +1,63 @@
+# -*- coding:utf-8 -*-
+from mako import runtime, filters, cache
+UNDEFINED = runtime.UNDEFINED
+STOP_RENDERING = runtime.STOP_RENDERING
+__M_dict_builtin = dict
+__M_locals_builtin = locals
+_magic_number = 10
+_modified_time = 1465565694.245934
+_enable_loop = True
+_template_filename = 'C:/Data/chf_postgresql/homepage/templates/base.htm'
+_template_uri = 'base.htm'
+_source_encoding = 'utf-8'
+import os, os.path, re, json
+_exports = ['content']
+
+
+from django_mako_plus import get_template_css, get_template_js 
+
+def render_body(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        __M_locals = __M_dict_builtin(pageargs=pageargs)
+        request = context.get('request', UNDEFINED)
+        self = context.get('self', UNDEFINED)
+        def content():
+            return render_content(context._locals(__M_locals))
+        __M_writer = context.writer()
+        __M_writer('\r\n')
+        __M_writer('\r\n\r\n\r\n<!DOCTYPE html>\r\n<html>\r\n  <meta charset="UTF-8">\r\n  <head>\r\n\r\n    <title>homepage</title>\r\n\r\n')
+        __M_writer('    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>\r\n\r\n')
+        __M_writer('    ')
+        __M_writer(str( get_template_css(self, request, context) ))
+        __M_writer('\r\n\r\n  </head>\r\n  <body>\r\n\r\n    <header>\r\n      Welcome to the homepage app!\r\n    </header>\r\n\r\n    ')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
+            context['self'].content(**pageargs)
+        
+
+        __M_writer('\r\n\r\n')
+        __M_writer('    ')
+        __M_writer(str( get_template_js(self, request, context) ))
+        __M_writer('\r\n\r\n  </body>\r\n</html>\r\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_content(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def content():
+            return render_content(context)
+        __M_writer = context.writer()
+        __M_writer('\r\n      Site content goes here in sub-templates.\r\n    ')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+"""
+__M_BEGIN_METADATA
+{"source_encoding": "utf-8", "filename": "C:/Data/chf_postgresql/homepage/templates/base.htm", "line_map": {"32": 18, "33": 18, "38": 29, "39": 32, "40": 32, "41": 32, "47": 27, "17": 4, "19": 0, "53": 27, "59": 53, "28": 2, "29": 4, "30": 15, "31": 18}, "uri": "base.htm"}
+__M_END_METADATA
+"""
