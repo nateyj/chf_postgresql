baeb59387f63280607c01ecc5a41ae7325670cc4
Johnson <nathan.johnson2@honeywell.com>
2016-06-10 09:25:25 -0400

    Modified settings.py to work with DMP

--- chf_postgresql/settings.py
+++ chf_postgresql/settings.py
@@ -24,6 +24,30 @@ SECRET_KEY = 'emg#a0=d7*zw-e6@^uea9p15tyjh_8$pi+$%63@_8c0wm!m&_#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+DEBUG_PROPAGATE_EXCEPTIONS = DEBUG  # never set this True on a live site
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django_mako_plus': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +61,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_mako_plus',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -48,21 +73,60 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_mako_plus.RequestInitMiddleware',
 ]
 
 ROOT_URLCONF = 'chf_postgresql.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'BACKEND': 'django_mako_plus.MakoTemplates',
         'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+            # functions to automatically add variables to the params/context before templates are rendered
+            'CONTEXT_PROCESSORS': [
+                'django.template.context_processors.static',            # adds "STATIC_URL" from settings.py
+                'django.template.context_processors.request',           # adds "request" object
+                'django.contrib.auth.context_processors.auth',          # adds "user" and "perms" objects
+                'django_mako_plus.context_processors.settings',         # adds "settings" dictionary
+            ],
+
+            # identifies where the Mako template cache will be stored, relative to each template directory
+            'TEMPLATES_CACHE_DIR': '.cached_templates',
+
+            # the default app and page to render in Mako when the url is too short
+            'DEFAULT_PAGE': 'index',
+            'DEFAULT_APP': 'homepage',
+
+            # the default encoding of template files
+            'DEFAULT_TEMPLATE_ENCODING': 'utf-8',
+
+            # these are included in every template by default - if you put your most-used libraries here, you won't have to import them exlicitly in templates
+            'DEFAULT_TEMPLATE_IMPORTS': [
+                'import os, os.path, re, json',
+            ],
+
+            # see the DMP online tutorial for information about this setting
+            'URL_START_INDEX': 0,
+
+            # whether to send the custom DMP signals -- set to False for a slight speed-up in router processing
+            # determines whether DMP will send its custom signals during the process
+            'SIGNALS': True,
+
+            # whether to minify using rjsmin, rcssmin during 1) collection of static files, and 2) on the fly as .jsm and .cssm files are rendered
+            # rjsmin and rcssmin are fast enough that doing it on the fly can be done without slowing requests down
+            'MINIFY_JS_CSS': True,
+
+            # the name of the SASS binary to run if a .scss file is newer than the resulting .css file
+            # happens when the corresponding template.html is accessed the first time after server startup
+            # if DEBUG=False, this only happens once per file after server startup, not for every request
+            # specify the binary in a list below -- even if just one item (see subprocess.Popen)
+            #'SCSS_BINARY': [ '/usr/bin/env', 'scss', '--unix-newlines' ],
+            'SCSS_BINARY': None,
+
+            # see the DMP online tutorial for information about this setting
+            # it can normally be empty
+            'TEMPLATES_DIRS': [
+                # '/var/somewhere/templates/',
             ],
         },
     },
@@ -119,3 +183,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+ # SECURITY WARNING: this next line must be commented out at deployment
+ BASE_DIR,
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
