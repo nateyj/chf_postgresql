793e2f2f6148d9eb8ecd41c181aa6e019cd9d0e4
Johnson <nathan.johnson2@honeywell.com>
2016-06-14 16:32:48 -0400

    Changed STATIC_URL back to its original value of '/static/'

--- chf_postgresql/settings.py
+++ chf_postgresql/settings.py
@@ -212,7 +212,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
  # SECURITY WARNING: this next line must be commented out at deployment
