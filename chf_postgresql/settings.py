a1bfdaa0024bcfe5da19ed69dd6102de44b5d6bf
Johnson <nathan.johnson2@honeywell.com>
2016-06-14 13:57:05 -0400

    Added original template engine back into TEMPLATES list in settings.py; doing so should enable the admin stie to work

--- chf_postgresql/settings.py
+++ chf_postgresql/settings.py
@@ -81,6 +81,19 @@ ROOT_URLCONF = 'chf_postgresql.urls'
 
 TEMPLATES = [
     {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+    {
         'BACKEND': 'django_mako_plus.MakoTemplates',
         'OPTIONS': {
             # functions to automatically add variables to the params/context before templates are rendered
